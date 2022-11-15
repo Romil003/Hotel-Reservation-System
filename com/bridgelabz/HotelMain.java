ae0a0e4d5d7c74dc4a505cb297f0ab779b37f679
Romil003 <romilghadge33@gmail.com>
2022-11-15 16:07:39 +0530

    Added Hotel Reservation System UC5

--- com/bridgelabz/HotelMain.java
+++ com/bridgelabz/HotelMain.java
@@ -80,26 +80,54 @@ public class HotelMain {
     public static void main(String[] args) {
         HotelMain user = new HotelMain();
         List<Hotel> hotelList = new ArrayList<Hotel>();
-        hotelList.add(new Hotel("Lakewood",110,90));
-        hotelList.add(new Hotel("Bridgewood",160,60));
-        hotelList.add(new Hotel("Ridgewood",220,150));
+        hotelList.add(new Hotel("Lakewood",110,90,0));
+        hotelList.add(new Hotel("Bridgewood",160,60,0));
+        hotelList.add(new Hotel("Ridgewood",220,150,0));
 
-        hotelList.stream().forEach(x -> System.out.println(x));
+//        hotelList.stream().forEach(x -> System.out.println(x));
 
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter start date : ");
-        String startDate = input.next();
-        System.out.println("Enter end date : ");
-        String endDate = input.next();
-        long totalWeekDays = getWeekdays(startDate,endDate);
-        System.out.println("Total weekDays => " + totalWeekDays);
-        System.out.println();
-        long totalWeekEnds = getWeekends(startDate,endDate);
-        System.out.println("Total weekEnds => " + totalWeekEnds);
+        int choice;
 
-        Hotel hotel1 = hotelList.get(0);
-        Hotel cheaperHotel = hotelList.stream().reduce(hotel1,(a,b) -> a.totalRate(totalWeekDays,totalWeekEnds)< b.totalRate(totalWeekDays,totalWeekEnds) ? a : b);
-        System.out.println("Cheaper hotel you get => " + cheaperHotel);
+        do {
+            System.out.print("1.View Hotels\t 2.Find cheaper Hotel\t 3.Add Ratings\t 4.Exit\n");
+            System.out.println("Enter a choice : ");
+            choice = input.nextInt();
+            switch (choice) {
+                case 1:
+                    System.out.println("Hotels are : ");
+                    hotelList.stream().forEach(x -> System.out.println(x));
+                    break;
+                case 2:
+                    System.out.println("Enter start date : ");
+                    String startDate = input.next();
+                    System.out.println("Enter end date : ");
+                    String endDate = input.next();
+
+                    long totalWeekDays = getWeekdays(startDate, endDate);
+                    System.out.println("Total weekDays => " + totalWeekDays);
+                    System.out.println();
+                    long totalWeekEnds = getWeekends(startDate, endDate);
+                    System.out.println("Total weekEnds => " + totalWeekEnds);
+
+                    Hotel hotel1 = hotelList.get(0);
+                    Hotel cheaperHotel = hotelList.stream().reduce(hotel1, (a, b) -> a.totalRate(totalWeekDays, totalWeekEnds) < b.totalRate(totalWeekDays, totalWeekEnds) ? a : b);
+                    System.out.println("Cheaper hotel you get => " + cheaperHotel);
+                    break;
+                case 3:
+                    System.out.println("Enter the Hotel name : ");
+                    String hotelName = input.next();
+                    hotelList.stream().filter(x -> x.getHotelName().equals(hotelName)).forEach(x -> {
+                        System.out.println("Enter rating -> ");
+                        double a = input.nextDouble();
+                        x.setRating(a);
+                    });
+                    break;
+                case 4:
+                    System.out.println("Exiting from program !!!!");
+                    break;
+            }
+        }while (choice != 4);
 
     }
 }
